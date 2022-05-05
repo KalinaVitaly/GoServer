cf9ff259600b19f579f48705db406866f4eb159e
Kalina Vitaly <67585869+KalinaVitaly@users.noreply.github.com>
2022-05-05 23:46:29 +0300

    Added timer file system

--- internal/app/apifilesystem/file_system_timer.go
+++ internal/app/apifilesystem/file_system_timer.go
@@ -2,10 +2,30 @@ package apifilesystem
 
 import (
 	"fmt"
+	"log"
+	"strconv"
 	"time"
 )
 
-func CallAtTimeToCreateDir(homeDirPath string, count, hour, min, sec int, timeToSleep int64, callerFunction func() error) error {
+func setCurrentContentDirPathAtTime(config *ConfigDirectories) error {
+	err := callAtTime(config, 0, 59, 59, 1, SetFilePathDirToSave)
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+	return nil
+}
+
+func createDirictoriesAtTime(config *ConfigDirectories) error {
+	err := callAtTime(config, 23, 59, 59, 24, CreateDirectoriesInFileSystem)
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+	return nil
+}
+
+func callAtTime(config *ConfigDirectories, hour, min, sec int, timeToSleep int64, callerFunction func(config *ConfigDirectories) error) error {
 	loc, err := time.LoadLocation("Local")
 
 	if err != nil {
@@ -14,7 +34,7 @@ func CallAtTimeToCreateDir(homeDirPath string, count, hour, min, sec int, timeTo
 
 	now := time.Now().Local()
 	firstCallTime := time.Date(
-		now.Year(), now.Month(), now.Day(), 23, 59, 30, 0, loc)
+		now.Year(), now.Month(), now.Day(), hour, min, sec, 0, loc)
 	if firstCallTime.Before(now) {
 		// Если получилось время раньше текущего, прибавляем сутки.
 		firstCallTime = firstCallTime.Add(time.Hour * time.Duration(timeToSleep))
@@ -22,20 +42,22 @@ func CallAtTimeToCreateDir(homeDirPath string, count, hour, min, sec int, timeTo
 
 	duration := firstCallTime.Sub(time.Now().Local())
 	fmt.Println("Duration : ", duration)
-	go func() {
+	go func(config *ConfigDirectories) {
 		time.Sleep(duration)
 		for {
-			callerFunction()
+			if err := callerFunction(config); err != nil {
+				log.Fatal(err)
+			}
 			time.Sleep(time.Hour * time.Duration(timeToSleep))
 		}
-	}()
+	}(config)
 
 	return nil
 }
 
-//func SetFilePathDirToSave() error {
-//	currentDate := time.Now().Format("01-02-2006")
-//	currentTime := time.Now().Hour()
-//	CurrentFilePath = CurrentHomeFilePath + currentDate + "_" + strconv.Itoa(currentTime) + "/"
-//	return nil
-//}
+func SetFilePathDirToSave(config *ConfigDirectories) error {
+	currentDate := time.Now().Format("01-02-2006")
+	currentTime := time.Now().Hour()
+	config.CurrentDirectoryPath = config.ContentFilePath + currentDate + "_" + strconv.Itoa(currentTime) + "/"
+	return nil
+}
--- internal/app/apifilesystem/work_with_directories.go
+++ internal/app/apifilesystem/work_with_directories.go
@@ -1,26 +1,42 @@
 package apifilesystem
 
-//
-//func CreateDirectoriesInFileSystem() error {
-//	var dirName string
-//	var dirPath string
-//	currentData := time.Now().Format("01-02-2006")
-//
-//	for i := int64(0); i < DirCountToCreate+1; i++ {
-//		dirName = currentData
-//		dirPath = CurrentHomeFilePath + "/"
-//		if i != 0 {
-//			dirName = currentData + "_" + strconv.Itoa(int(i))
-//			dirPath += currentData + "/"
-//		}
-//
-//		errorDirCreate := createDirInFileSystem(dirPath, dirName)
-//
-//		if errorDirCreate != nil {
-//			log.Fatal("Error: create dir failed!")
-//			panic(errorDirCreate)
-//		}
-//	}
-//
-//	return nil
-//}
+import (
+	"log"
+	"os"
+	"strconv"
+	"time"
+)
+
+func CreateDirectoriesInFileSystem(config *ConfigDirectories) error {
+	var dirName string
+	var dirPath string
+	currentData := time.Now().Format("01-02-2006")
+
+	for i := 0; i < config.DirectoriesCount+1; i++ {
+		dirName = currentData
+		dirPath = config.ContentFilePath
+		if i != 0 {
+			dirName = currentData + "_" + strconv.Itoa(int(i))
+			dirPath += currentData + "/"
+		}
+
+		errorDirCreate := createDirInFileSystem(dirPath, dirName)
+
+		if errorDirCreate != nil {
+			log.Fatal("Error: create dir failed!")
+
+		}
+	}
+
+	return nil
+}
+
+func createDirInFileSystem(filePath, dirName string) error {
+	err := os.Mkdir(filePath+dirName, 444)
+	if err != nil {
+		log.Fatal("Create dir error")
+		return err
+	}
+
+	return err
+}
